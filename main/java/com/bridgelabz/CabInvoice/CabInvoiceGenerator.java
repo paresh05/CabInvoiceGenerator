906e60bed7c34b3bf871606f7d236d11c0743824
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-09-28 11:12:22 +0530

    Added: Welcome Message

---
+++ main/java/com/bridgelabz/CabInvoice/CabInvoiceGenerator.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.CabInvoice;
+
+/**
+ * @author paresh.praveen_ymedi
+ *
+ */
+public class CabInvoiceGenerator {
+	public static void main(String[] args) {
+		System.out.println("Welcome to CAB INVOICE GENERATOR program");
+	}
+} 
